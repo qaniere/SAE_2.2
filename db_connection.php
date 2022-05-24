b2204cf434852115c2f1c9810da85cbdfe422cc7
Quentin ANIERE <quentin.aniere@gmail.com>
2022-05-24 15:00:06 +0200

    Add comments to the db connection script

--- db_connection.php
+++ db_connection.php
@@ -1,15 +1,21 @@
 <?php 
 
-    $file = fopen(stream_resolve_include_path(".env"), "r");
+    $file = fopen(stream_resolve_include_path(".env"), "r"); 
+    //stream_resolve_include_path find the good path for .env file
 
-    if($file == false) {
+    if($file == false) { 
+    //If a error occurs during the opening of the file
+
         echo ("Can't load .env file !");
         die();
     }
 
     while(($line = fgets($file)) !== false) {
-        $values = explode("=", $line);
-        ${$values[0]} = substr_replace($values[1] ,"",-1);
+    //Iterate over every line of the file
+
+        $values = explode("=", $line); //Transform a string "x=y" to an array like [x, y]
+        ${$values[0]} = substr_replace($values[1] , "", -1); //Transform the array [x, y] to $x = y
+        //The substr remove the space at the end of the line
         
     }
 
@@ -19,7 +25,7 @@
         $db = mysqli_connect($DB_URL, $DB_LOGIN, $DB_PASSWORD, $DB_NAME);
     
     } catch (Exception $e) {
-        echo "Echec";
+        echo "Échec : $e";
         die();
     }
 ?>
