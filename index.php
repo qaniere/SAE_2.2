805c78bfac17bdb5101be5702b03ee221677e223
Quentin ANIERE <quentin.aniere@gmail.com>
2022-05-26 19:26:43 +0200

    Start a basic index page

--- index.php
+++ index.php
@@ -1,63 +1,64 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <title>Coff-IT</title>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Coff-IT</title>
+    <link rel="shortcut icon" href="./img/logo.png" type="image/x-icon">
+    <link rel="stylesheet" href="./style/index.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+   
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@700&display=swap" rel="stylesheet">
 </head>
 <body>
-    <style>
-        body {
-            height: 95vh;
-            display: flex;
-            flex-wrap: wrap;
-            justify-content: center;
-            align-items: center;
-            background-color: #08bc94;
-        }
-
-        #logo-container {
-            height: 20em;
-            width: 20em;
-            padding: 3%;
-            border-radius: 50%;
-            background-color: #045e49;
-        }
-
-        img {
-            height: 20em;
-            width: 20em;
-        }
-
-        span {
-            text-align: center;
-            width: 100%;
-            font-size: 5vh;
-            font-family: 'Montserrat', sans-serif;
-        }
-    </style>
     <div id="logo-container">
-        <img src="logo.png">
+        <div id="account-pill">
+            <img id="user-logo" src="./img/user.png">
+            <p>Vous n'êtes pas connecté</p>
+        </div>
+        <div id="logo-shadow">
+            <img id="logo" src="./img/logo.png" alt="Le logo de l'entreprise qui représente un ordinateur posé sur des feuilles">
+        </div> 
+        <div id="arrow-container">
+            <div id="round">
+                <a href="#start"><img id="arrow" src="./img/arrow.png" alt="Une flèche qui sert à descendre dans la page"></a>
+            </div>
+        </div>
     </div>
-    <span id="text">Coming soon...</span>
-    <script>
-
-        const t = document.getElementById("text");
-        var index = 0;
-        var texts = ["Coming soon", "Coming soon.", "Coming soon..", "Coming soon..."]
-
-        console.log("coff-it");
-        window.setInterval(function(){
-            index++
-            if(index > texts.length - 1) {
-                index = 0;
-            }
-            t.innerText = texts[index];
-        }, 500);
-    </script>
+    <div id="start">
+        <h2>Qui sommes nous ? </h2>
+        <p>
+            Lyft hell of humblebrag organic shaman authentic, 
+            selvage vegan thundercats meh cloud bread poutine pop-up gentrify bushwick. 
+            Pinterest vegan yes plz irony vape hoodie blog hexagon gentrify four loko. Authentic 
+            gastropub enamel pin fanny pack banjo artisan succulents. Gastropub gentrify kickstarter, 
+            four loko heirloom authentic disrupt small batch +1. Pok pok stumptown pug meditation coloring 
+            book tattooed twee aesthetic banh mi helvetica. Hella meggings neutra affogato, direct trade prism 
+            kinfolk slow-carb migas cold-pressed ennui.
+        </p>
+        <h2>Des offres pour les particuliers mais aussi pour les entreprises !</h2>
+        <p>
+            <h3>Vous êtes un particulier :</h3>
+            Lyft hell of humblebrag organic shaman authentic, 
+            selvage vegan thundercats meh cloud bread poutine pop-up gentrify bushwick. 
+            Pinterest vegan yes plz irony vape hoodie blog hexagon gentrify four loko. Authentic 
+            gastropub enamel pin fanny pack banjo artisan succulents. Gastropub gentrify kickstarter, 
+            four loko heirloom authentic disrupt small batch +1. Pok pok stumptown pug meditation coloring 
+            book tattooed twee aesthetic banh mi helvetica. Hella meggings neutra affogato, direct trade prism 
+            kinfolk slow-carb migas cold-pressed ennui.
+        </p>
+        <p>
+            <h3>Vous êtes une entreprise :</h3>
+            Lyft hell of humblebrag organic shaman authentic, 
+            selvage vegan thundercats meh cloud bread poutine pop-up gentrify bushwick. 
+            Pinterest vegan yes plz irony vape hoodie blog hexagon gentrify four loko. Authentic 
+            gastropub enamel pin fanny pack banjo artisan succulents. Gastropub gentrify kickstarter, 
+            four loko heirloom authentic disrupt small batch +1. Pok pok stumptown pug meditation coloring 
+            book tattooed twee aesthetic banh mi helvetica. Hella meggings neutra affogato, direct trade prism 
+            kinfolk slow-carb migas cold-pressed ennui.
+        </p>
+    </div>
 </body>
 </html>
