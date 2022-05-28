ac99f68861c2a2962be443c206a2b91f69e590a7
Lucie Hrabal <hraballucie@gmail.com>
2022-05-28 16:43:10 +0200

    did the catalog and a start of front end, reworked itemdisplay a little.

--- common/catalog.php
+++ common/catalog.php
@@ -1,6 +1,47 @@
-<!-- liste des items ici -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./catalog.css">
+    <title>Catalogue de nos articles</title>
+</head>
+<body>
+    
+    
+    
+    <form action="./buy/itemDisplay.php" method="GET">
+    <h1>Catalogue de nos articles</h1>
+    <div class="container">
+        <?php
+            include "../db_connection.php";
 
-<form action="./buy/itemDisplay.php" method="GET">
-    <button value="1" name="item" type="submit">Voir</button>
-    <button value="2" name="item" type="submit">Voir</button>
-</form>
+            $stmt = $db->prepare("SELECT MAX(id) AS MaxId FROM TypeItem");
+            $stmt -> execute();
+            $rep = $stmt ->get_result();
+            $row = $rep -> fetch_assoc();
+
+            $maxId = $row["MaxId"];
+
+            //affiche chaque article disponible classés par id
+            for ($i = 1; $i <= $maxId; $i++) {
+                $stmt = $db->prepare("SELECT name FROM TypeItem WHERE id = ?");
+                $stmt -> bind_param("i",$i);
+                $stmt -> execute();
+
+                $rep = $stmt->get_result();
+                $row = $rep->fetch_assoc();
+
+                $item = $row["name"];
+                echo '<div class="article">';
+                echo "<div class='item'><h3>$item</h3></div>";
+                echo '<div class="item"><img src="./img/' . $i . '.jpg" width="300"></div>';
+                echo '<div class="item"><button value="' . $i . '" name="item" type="submit" class="button">Voir</button></div>';
+                echo '</div>';
+            }
+        ?>
+    </div>
+    </form>
+</body>
+</html>
