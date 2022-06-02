d908543f38fbe00754fa0e182d5842244b099030
Quentin ANIERE <quentin.aniere@gmail.com>
2022-06-02 21:19:33 +0200

    Fix catalog and item display (perfomances, cleanliness)

--- common/item_display.php
+++ common/item_display.php
@@ -2,83 +2,83 @@
 
     if (isset($_GET["item"])) {
         $id = $_GET["item"];
+
     } else {
         die("Aucun article séléctionné.");
     }
 
-    include "../db_connection.php";
+    include_once("../db_connection.php");
 
-    //vérifie si l'article demandé existe
+    //Check if the item exists
     $exists = $db->query("SELECT typeItem FROM BusinessSell WHERE typeItem = $id");
     $row = $exists->fetch_assoc();
     if (!isset($row["typeItem"])) {
         die("Cet article n'est pas en vente actuellement.<br><a href='../catalog.php'>Retourner au catalogue</a>");
     }
 
-    //Récupère le nom de l'objet, les entreprises qui le vendent, et à quel prix et les affiche
+   //Get the item's name, his price, the quantity available and the vendors
     $stmt = $db -> prepare("SELECT TypeItem.name AS ItemName, price, quantity, Business.name AS BusinessName FROM TypeItem,BusinessSell,Business WHERE TypeItem.id = ? AND TypeItem.id = typeItem AND Business.id = BusinessSell.business");
     $stmt -> bind_param("i",$id);
     $stmt -> execute();
 
-    $rep = $stmt -> get_result();
-    $row = $rep -> fetch_assoc();
+    $result = $stmt -> get_result();
+    $row = $result -> fetch_assoc();
     
     $name = $row["ItemName"];
     echo "$name<br><br>";
 
-    $maxQuantity = 0;
+    $max_quantity = 0;
     $quantity = $row["quantity"];
-    if ($quantity > $maxQuantity) $maxQuantity = $quantity; 
+    if ($quantity > $max_quantity) $max_quantity = $quantity; 
 
     $business = $row["BusinessName"];
     $price = $row["price"];
     echo "$quantity articles proposés par $business à $price €<br><br>";
 
-    while ($row = $rep -> fetch_assoc()) {
-
+    while($row = $result -> fetch_assoc()) {
         $quantity = $row["quantity"];
         $business = $row["BusinessName"];
         $price = $row["price"];
         echo "$quantity articles proposés par $business à $price €<br><br>";
     }
 
-    //photo de l'article
+    //Item picture
     echo "<img src='../uploaded_files/" . $id . ".jpg' height='400'><br>";
     
-    //affiche chaque attribut avec sa valeur
+    //Display each attributes of the item.
     $stmt = $db -> prepare("SELECT attribute,value FROM TypeItemDetails WHERE typeItem = ?");
     $stmt -> bind_param("i",$id);
     $stmt -> execute();
 
-    $rep = $stmt -> get_result();
-    $row = $rep -> fetch_assoc();
+    $result = $stmt -> get_result();
+    $row = $result -> fetch_assoc();
 
     $attribute = $row["attribute"];
     $value = $row["value"];
     echo "$attribute : $value<br>";
 
-    while ($row = $rep -> fetch_assoc()) {
+    while ($row = $result -> fetch_assoc()) {
         $attribute = $row["attribute"];
         $value = $row["value"];
         echo "$attribute : $value<br>";
     }
 ?>
 
-<!-- Un formulaire pour commander l'item -->
+<!-- an form to order an item -->
 <br>
 <form action="a preciser" method="POST">
     <label for="">Nombre d'articles : </label>
-    <input type="number" value="1" min="1" max=<?php echo $maxQuantity?> name="nbArticles">
+    <input type="number" value="1" min="1" max=<?php echo $max_quantity?> name="nbArticles">
     <select name="" id="">
         <?php
 
-            //affiche le nom des entreprises vendant le produit dans un menu déroulant, pour choisir chez quelle entreprise acheter le produit
+            //Display the vendors in a dropdown list to let the user to choose
             $stmt = $db -> prepare("SELECT name FROM Business,BusinessSell WHERE BusinessSell.business = Business.id AND BusinessSell.typeItem = ?");
             $stmt -> bind_param("i",$id);
             $stmt -> execute();
-            $rep = $stmt -> get_result();
+            $result = $stmt -> get_result();
 
-            while ($row = $rep -> fetch_assoc()) {
+            while ($row = $result -> fetch_assoc()) {
                 $business = $row["name"];
                 echo "<option>$business</option>";
             }
