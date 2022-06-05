315f19e445919ce5d50de850163a340066c33596
Quentin ANIERE <quentin.aniere@gmail.com>
2022-06-05 12:38:07 +0200

    Fix a responsive bug

--- scripts/index.js
+++ scripts/index.js
@@ -3,19 +3,21 @@ const PILL = document.getElementById("account-pill");
 const PILL_CONTENT = document.getElementById("pill-content");
 
 PILL.addEventListener("click", () => {
+
+    if(window.innerWidth < 1024) {
+        if(isPillVisible) {
+            isPillVisible = false;
+            PILL_CONTENT.style.display = "none";
+            PILL.style.width = "10vw";
+            PILL.style.borderRadius = "100%";
     
-    if(isPillVisible) {
-        isPillVisible = false;
-        PILL_CONTENT.style.display = "none";
-        PILL.style.width = "10vw";
-        PILL.style.borderRadius = "100%";
-
-    } else {
-
-        isPillVisible = true;
-        PILL.style.width = "auto";
-        PILL.style.borderRadius = "30px";
-        PILL_CONTENT.style.display = "flex";
-
+        } else {
+    
+            isPillVisible = true;
+            PILL.style.width = "auto";
+            PILL.style.borderRadius = "30px";
+            PILL_CONTENT.style.display = "flex";
+    
+        }
     }
 });
