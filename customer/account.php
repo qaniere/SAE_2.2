9eba793e14efb875009c49e25dbd7d94f0bbd4eb
Quentin ANIERE <quentin.aniere@gmail.com>
2022-06-08 21:51:27 +0200

    Add style for account

--- customer/account.php
+++ customer/account.php
@@ -9,90 +9,117 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Votre compte</title>
     <link href="../style/basic.css" rel="stylesheet">
+    <link href="../style/account.css" rel="stylesheet">
 </head>
 <body>
     <?php include_once("../include_files/menu.php");?>
-    <h1>Votre compte</h1>
-    <?php        
-        if(isset($_SESSION["id"])) {
+    <div id="account-container">
+        <h1>Votre compte</h1>
+         <?php        
+            if(isset($_SESSION["id"])) {
 
-            include_once("../include_files/db_connection.php");
-            
-            //Select stash and username
-            $stmt = $db -> prepare("SELECT stash FROM Customer WHERE id = ?");
-            $stmt -> bind_param("i", $_SESSION["id"]);
-            $stmt -> execute();
+                include_once("../include_files/db_connection.php");
+                
+                //Select informations of customer
+                $stmt = $db ->prepare("SELECT * FROM Customer JOIN CustomerProtectedData ON Customer.id = CustomerProtectedData.id  WHERE Customer.id = ?");
+                $stmt ->bind_param("i", $_SESSION["id"]);
+                $stmt ->execute();
 
-            $result = $stmt ->get_result();
-            $row = $result ->fetch_assoc();
+                $result = $stmt ->get_result();
+                $row = $result ->fetch_assoc();
 
-            $login = $_SESSION["login"];
-            $cagnotte = $row["stash"];
+                $login = $_SESSION["login"];
+                $stash = $row["stash"];
+                $email = $row["email"];
 
-            echo "Nom d'utilisateur : $login <br>Cagnotte fidélité : $cagnotte <br><br>";
+                echo "<h2>Vos informations</h2>";
+                echo "<ul>";
+                echo "<li><strong>Login :</strong> $login</li>";
+                echo "<li><strong>Cagnotte fidelité :</strong> $stash</li>";
+                echo "<li><strong>Adresse email :</strong> $email</li>";
+                echo "</ul>";
 
-            //Get all elements extracted by the customer
-            $stmt = $db -> prepare("SELECT name, quantity FROM Mendeleiev, CustomerExtraction WHERE Customer = ? AND Z = element");
-            $stmt -> bind_param("i", $_SESSION["id"]);
-            $stmt -> execute();
+                //Get all elements extracted by the customer
+                $stmt = $db ->prepare("SELECT name, quantity FROM Mendeleiev, CustomerExtraction WHERE Customer = ? AND Z = element");
+                $stmt ->bind_param("i", $_SESSION["id"]);
+                $stmt ->execute();
 
-            $result = $stmt -> get_result();
-            while ($row = $result -> fetch_assoc()) {
+                $result = $stmt -> get_result();
+                while ($row = $result -> fetch_assoc()) {
+                    $element = $row["name"];
+                    $quantity = $row["quantity"];
+                    echo "$element : $quantity<br>";
+                }
 
-                $element = $row["name"];
-                $quantity = $row["quantity"];
-                echo "$element : $quantity<br>";
-            }
+                //Get number of order of the customer
+                $stmt = $db -> prepare("SELECT COUNT(id) FROM CustomerOrder WHERE CustomerID = ?");
+                $stmt -> bind_param("i", $_SESSION["id"]);
+                $stmt -> execute();
+                $result = $stmt -> get_result();
+                $row = $result -> fetch_assoc();
+                $max = $row["COUNT(id)"]/10 % 10;
 
-            //Getting the value of the number of order
-            $stmt = $db -> prepare("SELECT COUNT(id) FROM CustomerOrder WHERE CustomerID = ?");
-            $stmt -> bind_param("i", $_SESSION["id"]);
-            $stmt -> execute();
-            $result = $stmt -> get_result();
-            $row = $result -> fetch_assoc();
-            $max = $row["COUNT(id)"]/10 % 10;
+                //Setting the offset for pagination
+                $offset = 0;
+                if (isset($_GET["history"])) {
 
-            //Setting the offset
-            $offset = 0;
-            if (isset($_GET["history"])) {
-                if ($_GET["history"] == "previous") {
-                    if ($offset!=0) {
-                        $offset -= 10;
-                    }
+                    if ($_GET["history"] == "previous") {
+                        if ($offset != 0) {
+                            $offset -= 10;
+                        }  
                     
-                }
-                else if ($_GET["history"] == "next") {
-                    if ($offset < $max) {
-                        $offset += 10;
+                    } else if ($_GET["history"] == "next") {
+                        if ($offset < $max) {
+                            $offset += 10;
+                        }
                     }
                 }
-            }
-            
-            //Getting the order history
-            echo "<h3>Historique des achats</h3>";
-            $stmt = $db->prepare("SELECT CustomerOrder.id, TypeItem.name AS itemName, TypeItem.file_extension AS file_extension, Business.name AS businessName, CustomerOrder.quantity, price FROM CustomerOrder JOIN BusinessSell ON CustomerOrder.businessID = BusinessSell.business AND CustomerOrder.itemID = BusinessSell.typeItem JOIN Business ON CustomerOrder.businessID = Business.id JOIN TypeItem ON CustomerOrder.itemID = TypeItem.id WHERE CustomerID = ? LIMIT 10 OFFSET $offset");
-            $stmt->bind_param("i", $_SESSION["id"]);
-            $stmt->execute();
-            $result = $stmt->get_result();
-            while ($row = $result->fetch_assoc()) {
-                $itemID = $row["itemName"];
-                $businessID = $row["businessName"];
-                $quantity = $row["quantity"];
-                $price = $row["price"]*$quantity;
-                echo "Produit : $itemID <img src='../catalog_pictures/" . $row["id"] . "." .  $row["file_extension"] . "' width='50' height='50'>, Business : $businessID, Quantité : $quantity, Prix : $price €<br>";
-            }
-            ?>
+                
+                //Getting the order history
+                echo "<h3>Historique des achats</h3>";
+                echo "<table>";
+                echo "<tr>";
+                echo "<th>Numéro de commande</th>";
+                echo "<th>Nom de l'objet</th>";
+                echo "<th>Vendeur</th>";
+                echo "<th>Photo</th>";
+                echo "<th>Lien vers l'article</th>";
+                echo "</tr>";
 
-            
-            <form action="../customer/account.php" method="get">
-                <button type="submit" name="history" value="previous"><</button>
-                <button type="submit" name="history" value="next">></button>
-            </form>
+                $stmt = $db->prepare("SELECT CustomerOrder.id AS OrderID, TypeItem.id AS id, TypeItem.name AS itemName, TypeItem.file_extension AS file_extension, Business.name AS businessName, CustomerOrder.quantity, price FROM CustomerOrder JOIN BusinessSell ON CustomerOrder.businessID = BusinessSell.business AND CustomerOrder.itemID = BusinessSell.typeItem JOIN Business ON CustomerOrder.businessID = Business.id JOIN TypeItem ON CustomerOrder.itemID = TypeItem.id WHERE CustomerID = ? LIMIT 10 OFFSET ?");
+                $stmt->bind_param("ii", $_SESSION["id"], $offset);
+                $stmt->execute();
+                $result = $stmt->get_result();
 
-        <?php
-        } else {
-            echo "Vous n'êtes pas connecté. Allez sur <a href='login.php'>login.php</a>.";
-        }
-    ?>
+                while ($row = $result->fetch_assoc()) {
+                    $order_id = $row["OrderID"];
+                    $item_name = $row["itemName"];
+                    $business_name = $row["businessName"];
+                    $quantity = $row["quantity"];
+                    $price = $row["price"]*$quantity;
+                    $id = $row["id"];
+                    $file_extension = $row["file_extension"];
+
+                    echo "<tr>";
+                    echo "<td>$order_id</td>";
+                    echo "<td>$item_name</td>";
+                    echo "<td>$business_name</td>";
+                    echo "<td><img class='history-image'src='../catalog_pictures/$id.$file_extension'></td>";
+                    echo "<td><a href='../common/item_display.php?item=$id'>Voir l'article</a></td>";
+                    echo "</tr>";
+                }
+
+                echo "</table>";
+          
+                echo "<form action='../customer/account.php' method='get'>
+                    <button type='submit' name='history' value='previous'>< Page précédente</button>
+                    <button type='submit' name='history' value='next'>Page suivante ></button>
+                </form>";
+
+            } else {
+                echo "Vous n'êtes pas connecté. Allez sur <a href='login.php'>login.php</a>.";
+            }
+        ?>
+    </div>
 </body>
 </html>
