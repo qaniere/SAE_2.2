854656895365aa4edfe31b3563b14348d025949d
Quentin ANIERE <quentin.aniere@gmail.com>
2022-06-08 14:05:04 +0200

    Add style to 404 page

--- common/404.php
+++ common/404.php
@@ -8,12 +8,20 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../style/basic.css">
+    <link rel="stylesheet" href="../style/404.css">
     <title>Erreur 404</title>
 </head>
 <body>
     <?php include_once("../include_files/menu.php");?>
-
-        <h1>Erreur 404</h1>
-        <p>La page demandée n'existe pas !</p>
-        <a href="../index.php">Retourner à l'accueil</a>
+    <div id="page-content">
+        <div class="text-container">
+            <h1>Erreur 404</h1>
+        </div> 
+        <div class="text-container">
+            <p>La page demandée n'existe pas !</p>
+        </div>     
+        <div class="text-container">           
+            <a href="../index.php">Retourner à l'accueil</a>
+        </div>      
+    </div>
 </body>
