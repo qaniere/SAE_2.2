3ae2a303f822599a02c12266a105ce5b102b07a7
kamil <kamil21mrdc@gmail.com>
2022-06-08 15:05:07 +0200

    Fix checkout.php

--- customer/checkout.php
+++ customer/checkout.php
@@ -1,10 +1,14 @@
 <?php
     session_start();
-    include_once("../include_files/db_connection.php");
-    include_once("../include_files/menu.php");
+    
     if (!isset($_SESSION['login'])) {
-        header("Location: ../common/login.php");
+        header("Location: ../customer/login.php");
+        
     } else {
+
+        include_once("../include_files/db_connection.php");
+        include_once("../include_files/menu.php");
+
         //Select all items in the cart and update the database
         foreach ($_SESSION['cart'] as $id => $businessID) {
             foreach ($businessID as $quantity) {
@@ -22,8 +26,12 @@
 
         //Empty the cart
         unset($_SESSION['cart']);
+        ?>
+
+        <h1>Votre commande a été passée </h1>
+        <p> Merci d'avoir commandé chez nous, à la prochaine ! </p>
+
+    <?php
     }
 ?>
 
-<h1>Votre commande a été passée </h1>
-<p> Merci d'avoir commandé chez nous, à la prochaine ! </p>
